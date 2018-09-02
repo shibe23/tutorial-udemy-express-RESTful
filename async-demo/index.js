408dcef3fef72db2bc68f16528abb95981178bf9
shibe23 <shibe23.info@gmail.com>
2018-09-03 07:26:43 +0900

    Callbacks

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,11 +1,23 @@
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+  console.log('User', user);
+
+  getRepositories(user.gitHubUsername, (repos) => {
+    console.log('Repos', repos);
+  });
+});
 console.log('After');
 
-function getUser (id) {
+function getUser (id, callback) {
   setTimeout(() => {
     console.log('Reading a user from a database...');
-    return { id: id, gitHubUsername: 'hoge' };
+    callback({ id: id, gitHubUsername: 'hoge' });
+  }, 2000);
+}
+
+function getRepositories(username, callback) {
+  setTimeout(() => {
+    console.log('Calling Github API...');
+    callback(['repo1', 'repo2', 'repo3']);
   }, 2000);
 }
