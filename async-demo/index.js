7d43cb7963f44a965f266e4d1b2cc041f9d22654
shibe23 <shibe23.info@gmail.com>
2018-09-06 01:05:35 +0900

    Async and Await

--- async-demo/index.js
+++ async-demo/index.js
@@ -7,11 +7,27 @@ console.log('Before');
 //     });
 //   });
 // });
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log('Commits', commits))
-  .catch(err => console.log('Error', err.message));
+
+// Promise-based approach
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log('Commits', commits))
+//   .catch(err => console.log('Error', err.message));
+
+// Async and Await approach
+async function displayCommits() {
+  try{
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  }
+  catch(err){
+    console.log('Error', err.message);
+  }
+}
+displayCommits();
 
 console.log('After');
 
