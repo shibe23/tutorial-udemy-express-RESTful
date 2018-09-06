0b07e06f0e1ce5713157814623ac355fbe793b6c
shibe23 <shibe23.info@gmail.com>
2018-09-07 00:17:05 +0900

    Schemas

---
+++ mongo-demo/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+  .then(() => console.log('Connented to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...', err))
+
+  const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+  });
