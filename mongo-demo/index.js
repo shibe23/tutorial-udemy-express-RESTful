183df182012bc9b2124997c53133906832d46061
shibe23 <shibe23.info@gmail.com>
2018-09-08 00:19:33 +0900

    Logical Query Operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -29,11 +29,11 @@ async function createCourse() {
 async function getCourses(){
   const courses = await Course
     .find({ author: 'Mosh', isPublished: true })
-    .find({ price: 10 })
-    .find({ price: { $gte: 10, $lte: 20 } })
-    .find({ price: { $in: [10, 15, 20] } })
+    .or([ { author: 'Mosh'}, { isPublished: true } ])
+    .and([ { author: 'Mosh'}, { isPublished: true } ])
     .limit(10)
-    .sort({ name: 1, tags: 1 }); // 1: ascending order -1: desending order
+    .sort({ name: 1})
+    .select({ name: 1, tags: 1 })
   console.log(courses);
 }
 
